87abedd45d79cb12d7fc75d7ac70e65bf53cdd0c
Derick Sheldrick <sheldrickderick9@gmail.com>
2025-04-17 18:41:50 +0300

    state declaration

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,7 +4,15 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(count + 1);
+
+    // solving the delay with states we need to declare it asychronous way
+    // just the way I have done it dowwn here => setCount((count) => count + 1)
+    
+    // console.log(`before setState ${count}`);
+
+    setCount((count) => count + 1);
+
+    // console.log(`after setState ${count}`);
   }
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
